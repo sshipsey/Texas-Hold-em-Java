f80a6ca5d1f775a1bb0b76e297445a706a5d6420
Steve Shipsey <sashipsey@gmail.com>
2012-12-15 18:05:02 -0500

    Brining in existing code

---
+++ src/card.java
@@ -0,0 +1,13 @@
+public class card {
+	public int value = 0;
+	public char suit = 'S';
+	public String imgPath = "";
+	//constructor
+	public card(int a, char b, String c)
+	{
+		value = a;
+		suit = b;
+		c = imgPath;
+	}
+
+}
---
+++ src/window.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+public class window {
+	public window()
+	{
+		JFrame frame = new JFrame("Texas Hold 'Em");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null); 
+	    frame.setResizable(false);
+	    frame.setSize(1000,500);
+	    frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+	    frame.setVisible(true); 
+	}
+	
+    
+}
